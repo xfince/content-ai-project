f17070c0208dc6f9ce35dfd6f9cc30cd89ca9cbe
Kimani Vincent <kimanivincent2006@gmail.com>
2024-07-26 21:10:54 +0300

    trial-two

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 from flask_cors import CORS
 from content_generator import generate_content
 from content_moderator import moderate_content
@@ -27,4 +28,4 @@ def moderate():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
